b31a6b6d2dfb6c2ca793608b7c1143364c036a4b
Jon Friskics <jonfriskics@gmail.com>
2019-04-15 23:45:36 -0400

    module5-solution

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
 import QuizEnd from './QuizEnd.js'
+import { stat } from 'fs';
 
 let quizData = require('./quiz_data.json')
 
@@ -9,11 +10,16 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
   render() {
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
     return (
       <div>
-        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     )
   }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  handleClick(buttonText) {
+    if(buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler()
+    }
+  }
   render() {
     return (
       <main>
@@ -10,7 +15,9 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
+            })}
           </ul>
         </section>
       </main>      

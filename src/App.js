91b0d2e6a7bf37dc97b4fd210224233d5cbb035a
ZackWotawa <zach.wotawa@daugherty.com>
2019-07-24 16:32:28 -0500

    first module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
